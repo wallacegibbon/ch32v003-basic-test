ddd7053c2d7cc2356cd7c4151274c10135d39f9d
wallace <wallacegibbon@aliyun.com>
2023-03-06 14:24:56 +0800

    Tested on The CH32V003F4P6 Eval board from WCH. (Serial port RXNE interrupt and GPIO are both tested)

---
+++ Makefile
@@ -0,0 +1,25 @@
+CROSS_COMPILER_PREFIX = $(HOME)/MRS_Toolchain_Linux_x64_V1.60/RISC-V Embedded GCC/bin/riscv-none-embed-
+OPENOCD = /usr/local/bin/openocd
+OPENOCD_ARGS = -f interface/wlink.cfg -f target/wch-riscv.cfg
+
+#ARCH = -march=rv32imac -mabi=ilp32
+ARCH = -march=rv32ec -mabi=ilp32e
+
+CH32_STD_LIB_DIR = $(HOME)/playground/CH32_standard_library/ch32v00x
+
+CROSS_C_SOURCE_FILES += $(wildcard $(CH32_STD_LIB_DIR)/peripheral/src/*.c)
+CROSS_C_SOURCE_FILES += $(wildcard $(CH32_STD_LIB_DIR)/core/*.c)
+CROSS_C_SOURCE_FILES += $(wildcard ./src/*.c)
+
+CROSS_ASM_SOURCE_FILES += $(CH32_STD_LIB_DIR)/sample/startup.S
+
+CROSS_LINKER_SCRIPT = $(CH32_STD_LIB_DIR)/sample/default.ld
+
+CROSS_C_INCLUDES = \
+-I$(CH32_STD_LIB_DIR)/peripheral/inc -I$(CH32_STD_LIB_DIR)/core -I./src \
+
+OPENOCD_FLASH_COMMANDS = \
+-c "program $<" -c wlink_reset_resume -c exit
+
+include ./miscellaneous-makefiles/cross-gcc-mcu.mk
+
---
+++ src/main.c
@@ -0,0 +1,81 @@
+#include "ch32v00x.h"
+#include "debug.h"
+#include <stdio.h>
+
+int static_display = 0;
+int LED_value = 0;
+
+void initialize() {
+	NVIC_InitTypeDef NVIC_InitStructure;
+	USART_InitTypeDef USART_InitStructure;
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	RCC_APB2PeriphClockCmd(
+		RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOD | RCC_APB2Periph_USART1,
+		ENABLE
+	);
+
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+	USART_InitStructure.USART_BaudRate = 9600;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+
+	USART_Init(USART1, &USART_InitStructure);
+	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+
+	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+
+	USART_Cmd(USART1, ENABLE);
+}
+
+void loop() {
+	GPIO_WriteBit(GPIOC, GPIO_Pin_1, !LED_value);
+
+	if (!static_display)
+		LED_value = !LED_value;
+
+	Delay_Ms(200);
+}
+
+void main() {
+	Delay_Init();
+	initialize();
+	printf("System is ready now. SystemClk: %d\r\n", SystemCoreClock);
+
+	while (1)
+		loop();
+}
+
+__attribute__((interrupt("WCH-Interrupt-fast")))
+void USART1_IRQHandler() {
+	switch (USART_ReceiveData(USART1)) {
+	case '0':
+		static_display = 1;
+		LED_value = 0;
+		break;
+	case '1':
+		static_display = 1;
+		LED_value = 1;
+		break;
+	default:
+		static_display = 0;
+	}
+}
+
